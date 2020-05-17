eb8a8953bb22396fb7c11902caefdce4b2aeee54
Adnan Khan <adnankhan1705@gmail.com>
2020-05-17 14:14:34 +0530

    open Facebook
    Update code for opening facebook on chrome and browser

--- my_program.py
+++ my_program.py
@@ -1,6 +1,9 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
+import wikipedia
+import webbrowser
+import os
 from datetime import date
 
 
@@ -57,17 +60,39 @@ def takecommand():
 
 if __name__ == "__main__":
     date=date.today()
+    chromedir = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
     speak("Hi Adnan")
     wishMe()
     while True:
         query = takecommand().lower()
         if "name" in query and "my" in query:
             speak("Your name is Adnan Khan")
+
         if "name" in query and "your" in query:
             speak("My name is Jarvis")
 
-        if "exit" in query or "out" in query or "close" in query:
-            speak("Good Bye Adnan")
-            break
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        if 'open' in query and 'facebook' in query:
+            if 'chrome' in query:
+                webbrowser.get(chromedir).open("facebook.com")
+            else:
+                webbrowser.open("facebook.com")
+            
+
+        elif "exit" in query or "out" in query or "close" in query:
+            if 'internet explorer' in query:
+                os.system("TASKKILL /F /IM iexplore.exe")
+            elif 'chrome' in query:
+                os.system("TASKKILL /F /IM chrome.exe")
+            else:
+                speak("Good Bye Adnan")
+                break
         
 
